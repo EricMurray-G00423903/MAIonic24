5f5f29acc953abd20ad5c90dbb2dc4fb6d169c8d
EricMurray-G00423903 <g00423903@atu.ie>
2024-05-04 12:19:13 +0100

    Handled geolocation errors, finished UI for home page, routing done, geolocation done, weather api integrated, closes #4, closes #5, closes #6, closes #7, closes #8

--- MAWeather/src/app/home/home.page.ts
+++ MAWeather/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LoadingController, NavController } from '@ionic/angular';
 import { Geolocation } from '@capacitor/geolocation';
 import { environment } from 'src/environments/environment';
 
@@ -38,30 +39,75 @@ export class HomePage {
   cityName: any
   localTemp: any
   todaysDate = new Date()
-  weatherDetails: WeatherDetail[] = [];
+  weatherDetails: WeatherDetail[] = []
   weatherIcon: any
+  loading: any
 
-  constructor(public httpClient:HttpClient) {
+  constructor(public httpClient:HttpClient, public loadingController: LoadingController, private navCtrl: NavController) {
+    this.presentLoading()
     this.getCurrentLocation()
   }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Please wait...',
+      spinner: 'circles' // You can choose different spinner styles or customize it
+    });
+    await this.loading.present();
+  }
+
+  dismissLoading() {
+    if (this.loading) {
+      this.loading.dismiss();
+    }
+  }
+
   async getCurrentLocation() {
     try {
       const coordinates = await Geolocation.getCurrentPosition();
       this.pullData(coordinates.coords.latitude, coordinates.coords.longitude);
     } catch (e) {
       console.error('Error getting location', e);
+      this.dismissLoading();
+      this.setDefaultLocation();
     }
   }
+  
+
+  setDefaultLocation() {
+    const defaultLat = 53.3498; // Latitude for Dublin
+    const defaultLon = -6.2603; // Longitude for Dublin
+    this.pullData(defaultLat, defaultLon);
+  }
+  
 
   pullData(latitude: number, longitude: number) {
-    const API_KEY = environment.API_KEY;
-    const API_URL = environment.API_URL;
-    this.httpClient.get<WeatherResponse>(`${API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`).subscribe(results => {
-      this.localTemp = results.main;
-      this.weatherDetails = results.weather;
-      this.cityName = results.name;
-      this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails[0].icon}@4x.png`;
-    });
+    this.httpClient.get<WeatherResponse>(`${API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`).subscribe(
+      results => {
+        this.localTemp = results.main;
+        this.weatherDetails = results.weather;
+        this.cityName = results.name;
+        this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails[0].icon}@4x.png`;
+        this.dismissLoading();
+      },
+      error => {
+        console.error('Failed to fetch weather data', error);
+        this.dismissLoading();
+      }
+    );
   }
+
+  navigateToSearch() {
+    this.navCtrl.navigateForward('/search');
+  }
+
+  navigateToSettings() {
+    this.navCtrl.navigateForward('/settings');
+  }
+
+  navigateToAbout() {
+    this.navCtrl.navigateForward('/about');
+  }
+  
 
 }
